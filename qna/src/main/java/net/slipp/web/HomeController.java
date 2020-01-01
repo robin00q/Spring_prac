f00eee21b7a99737ebb33f34e6eacb19f760f067
이석준 <robin00q@naver.com>
2020-01-01 12:57:19 +0900

    add logback dependency

---
+++ qna/src/main/java/net/slipp/web/HomeController.java
@@ -0,0 +1,18 @@
+package net.slipp.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/")
+	public String home() {
+		log.debug("logback success");
+		return "home";
+	}
+}
