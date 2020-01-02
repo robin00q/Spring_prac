da98055253c5eb8672ea0e3647b9562bf9efda31
이석준 <robin00q@naver.com>
2020-01-02 20:18:42 +0900

    add flyway database migration tools for management database

--- qna/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ qna/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -1,39 +1,15 @@
 package net.slipp.dao.users;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import net.slipp.domain.users.User;
 
 public class MyBatisUserDao implements UserDao {
 	
-	private static final Logger log = LoggerFactory.getLogger(MyBatisUserDao.class);
-	
 	private SqlSession sqlSession;
-	
-	private DataSource dataSource;
-	
-	@PostConstruct
-	public void initialize() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("slipp.sql"));
-		DatabasePopulatorUtils.execute(populator, dataSource);
-		log.info("database initialized success");
-	}
 	
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-	}
-	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
 	}
 
 	@Override
